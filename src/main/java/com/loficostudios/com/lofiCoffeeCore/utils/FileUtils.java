d54a5abd3e9d469e3f5e2ea3ea17fb40333c43b8
Logan <tonierbobcat966@gmail.com>
2025-01-22 17:54:06 -0800

    updated file utils

--- src/main/java/com/loficostudios/com/lofiCoffeeCore/utils/FileUtils.java
+++ src/main/java/com/loficostudios/com/lofiCoffeeCore/utils/FileUtils.java
@@ -1,5 +1,16 @@
 package com.loficostudios.com.lofiCoffeeCore.utils;
 
+import com.loficostudios.com.lofiCoffeeCore.LofiCoffeeCore;
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class FileUtils {
     public static String removeExtension(String fileName) {
         if (fileName == null || fileName.isEmpty()) {
@@ -13,5 +24,43 @@ public class FileUtils {
 
         return fileName; // Return original string if no period is found
     }
+    private static String serializeString(Object object) {
+        Logger lgr = LofiCoffeeCore.getInstance().getLogger();
+        try {
+            ByteArrayOutputStream io = new ByteArrayOutputStream();
+            BukkitObjectOutputStream outputStream = new BukkitObjectOutputStream(io);
+            outputStream.writeObject(object);
+            outputStream.flush();
+            byte[] obj = io.toByteArray();
 
+            return Base64.getEncoder().encodeToString(obj);
+        } catch (IOException e) {
+            lgr.log(
+                    Level.SEVERE,
+                    e.getMessage());
+            return null;
+        }
+    }
+    private static Object deserializeString(String string, Class<?> clazz) {
+        Logger lgr = LofiCoffeeCore.getInstance().getLogger();
+
+        byte[] obj = Base64.getDecoder().decode(string);
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(obj);
+
+        try {
+            BukkitObjectInputStream bukkitInputStream = new BukkitObjectInputStream(inputStream);
+            Object deserializedObj = bukkitInputStream.readObject();
+            if (clazz.isInstance(deserializedObj)) {
+                return clazz.cast(deserializedObj);
+            } else {
+                throw new ClassNotFoundException("Deserialized object is not of the expected type");
+            }
+        } catch (IOException e) {
+            lgr.log(Level.SEVERE, e.getMessage());
+            return null;
+        } catch (ClassNotFoundException ex) {
+            lgr .log(Level.SEVERE, "Cannot decode base64 byte array to ItemStack");
+            return null;
+        }
+    }
 }
